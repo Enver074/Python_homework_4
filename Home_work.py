9b09ea3ee26a0938a2e9e07116ab430cf4ca4fe5
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-12-26 23:08:29 +0300

    Второе задание

--- Home_work.py
+++ Home_work.py
@@ -16,8 +16,23 @@
     
 # print(round(math.pi, count))
 
-import math
+# import math
 
-d = int(input('Введите количество цифр после запятой: '))
+# d = int(input('Введите количество цифр после запятой: '))
 
-print(round(math.pi, d))
+# print(round(math.pi, d))
+
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+n = int(input("Введите натеральное число: "))
+f = []
+d = 2
+m = n
+while d * d <= n:
+    if n % d == 0:
+        f.append(d)
+        n//=d
+    else:
+        d += 1
+f.append(n)
+print(f'{m} = {f}') 
