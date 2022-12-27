adaa93b779da5ab3432ff328987d4bf100f69168
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-12-27 15:43:07 +0300

    Четверьтое задание

--- Home_work.py
+++ Home_work.py
@@ -40,15 +40,58 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
 
-a = []
-list = []
+# a = []
+# list = []
 
-for element in input('Введите последовательность чисел: ').split(','):
-    a.append(int(element))
+# for element in input('Введите последовательность чисел: ').split(','):
+#     a.append(int(element))
     
-for i in a:
-    if i not in list:
-        list.append(i)
+# for i in a:
+#     if i not in list:
+#         list.append(i)
 
-print(a)
-print(list)
+# print(a)
+# print(list)
+
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+ 
+print('Введите натуральную степень k:')
+k = int(input())
+ 
+def write_file(st):
+    with open('Task4.txt', 'w') as data:
+        data.write(st)
+ 
+def create_list(k):
+    list = []
+    for i in range(k + 1):
+        list.append(randint(0, 101))    
+    return list
+    
+def create_str(sp):
+    list = sp[::-1]
+    func = ''
+    if len(list) < 1:
+        func = 'x = 0'
+    else:
+        for i in range(len(list)):
+            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
+                func += f'{list[i]}x^{len(list) - i - 1}'
+                if list [i + 1] != 0:
+                    func += ' + '
+            elif i == len(list) - 2 and list[i] != 0:
+                func += f'{list[i]}x'
+                if list[i + 1] != 0:
+                    func += ' + '
+            elif i == len(list) - 1 and list[i] != 0:
+                func += f'{list[i]} = 0'
+            elif i == len(list) - 1 and list[i] == 0:
+                func += ' = 0'
+    return func
+ 
+koef = create_list(k)
+write_file(create_str(koef))
